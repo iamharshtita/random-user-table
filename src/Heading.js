bc5a0507e37141b1da27de40141795483a2b2b26
Harsh <harshtita01@gmail.com>
2022-03-13 01:42:16 +0530

    Create a React app and publish it to GitHub Pages

---
+++ src/Heading.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './Heading.css'
+function Heading(){
+
+    return(
+        <div className="heading"><h1>|| Random User Table ||</h1></div>
+    )
+}
+
+export default Heading;
